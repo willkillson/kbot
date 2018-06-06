11f5d8812f76fad329d0df63a6a2a167b48a2eee
kevin wilkinson <willkillson@gmail.com>
2018-06-06 15:25:46 -0700

    Initial commit

---
+++ src/com/company/tests/MainTest.java
@@ -0,0 +1,13 @@
+import com.company.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+@Test
+    public void testMain(){
+
+    assertTrue(false);
+
+    }
+}
