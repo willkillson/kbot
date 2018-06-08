783c2f088ad0cbce7e2b770f31a81bfca37c235f
kevin wilkinson <willkillson@gmail.com>
2018-06-07 17:19:19 -0700

    Encrypting complete for spiral clockwise

--- src/com/company/Cipher.java
+++ src/com/company/Cipher.java
@@ -8,24 +8,31 @@ public class Cipher {
     public String plainText;
     public int demX;
     public int demY;
-    public String rotation;
     public char[][] cipherArray;
+    public boolean isRotateRight = false;
 
 
-    public String cipherText;
+    public String cipherText = "";
 
 
 
     public Cipher(String input){
 
-        Pattern pattern = Pattern.compile("\"(.*)\" [(](\\d), (\\d)[)] (.*)");
+        Pattern pattern = Pattern.compile("\"(.*)\" [(](\\d+), ?(\\d+)[)] ?(.*)");
         Matcher matcher = pattern.matcher(input);
         matcher.find();
 
         this.plainText = matcher.group(1);
         this.demX = Integer.parseInt(matcher.group(2));
         this.demY = Integer.parseInt(matcher.group(3));
-        this.rotation = matcher.group(4);
+        String rotation = matcher.group(4);
+
+        if(rotation.equals("clockwise")){
+            this.isRotateRight = true;
+        }
+        else{
+            this.isRotateRight = false;
+        }
     }
 
     public void init(){
@@ -34,6 +41,8 @@ public class Cipher {
         plainText = plainText.replaceAll("[.]*+", "");
         plainText = plainText.replaceAll("[?]*+", "");
         plainText = plainText.replaceAll("[!]*+", "");
+        plainText = plainText.replaceAll("[']*+", "");
+        plainText = plainText.replaceAll("[-]*+", "");
 
         this.plainText = this.plainText.toUpperCase();
         this.cipherArray = new char[this.demY][this.demX];
@@ -47,11 +56,19 @@ public class Cipher {
                 }
             }
         }
-
-
-
     }
 
+    public void encryptString(){
+
+        CrypterCar car = new CrypterCar(isRotateRight,demX,demY);
+        int index = 0;
+        cipherText+= cipherArray[0][demX-1];
+        while(index<demX*demY-1){
+            IntPair pair = car.moveRotateRight();
+            cipherText+= cipherArray[pair.y][pair.x];
+            index++;
+        }
+    }
 
     public void print(){
         for(int i = 0;i< this.demY;i++){
--- src/com/company/tests/TestCipher.java
+++ src/com/company/tests/TestCipher.java
@@ -11,7 +11,7 @@ public class TestCipher {
         assertEquals("WE ARE DISCOVERED. FLEE AT ONCE",testCipher.plainText);
         assertEquals(9,testCipher.demX);
         assertEquals(3,testCipher.demY);
-        assertEquals("clockwise",testCipher.rotation);
+        assertTrue(testCipher.isRotateRight);
     }
 
     @Test
@@ -50,10 +50,8 @@ public class TestCipher {
 
     }
 
-    
-
     @Test
-    public void TestCrypterCarExhaustive(){
+    public void TestCrypterCarExhaustiveRotationRight(){
 
         for(int i =2;i<=20;i++ ){
             for(int j = 2;j<=20;j++){
@@ -71,6 +69,60 @@ public class TestCipher {
         }
 
 
+    }
+
+    @Test
+    public void TestEncryptString1(){
+        Cipher testCipher = new Cipher("\"WE ARE DISCOVERED. FLEE AT ONCE\" (9, 3) clockwise");
+        testCipher.init();
+        testCipher.encryptString();
+
+        assertEquals("CEXXECNOTAEOWEAREDISLFDEREV", testCipher.cipherText);
+    }
+
+    @Test
+    public void TestEncryptString2(){
+        Cipher testCipher = new Cipher("\"why is this professor so boring omg\" (6, 5) counter-clockwise");
+        testCipher.init();
+        testCipher.encryptString();
+
+        assertEquals("TSIYHWHFSNGOMGXIRORPSIEOBOROSS", testCipher.cipherText);
+    }
+
+    @Test
+    public void TestEncryptString3(){
+        Cipher testCipher = new Cipher("\"Solving challenges on r/dailyprogrammer is so much fun!!\" (8, 6) counter-clockwise");
+        testCipher.init();
+        testCipher.encryptString();
+
+        assertEquals("CGNIVLOSHSYMUCHFUNXXMMLEGNELLAOPERISSOAIADRNROGR", testCipher.cipherText);
+    }
+
+    @Test
+    public void TestEncryptString4(){
+        Cipher testCipher = new Cipher("\"For lunch let's have peanut-butter and bologna sandwiches\" (4, 12) clockwise");
+        testCipher.init();
+        testCipher.encryptString();
+
+        assertEquals("LHSENURBGAISEHCNNOATUPHLUFORCTVABEDOSWDALNTTEAEN", testCipher.cipherText);
+    }
+
+    @Test
+    public void TestEncryptString5(){
+        Cipher testCipher = new Cipher("\"I've even witnessed a grown man satisfy a camel\" (9,5) clockwise");
+        testCipher.init();
+        testCipher.encryptString();
+
+        assertEquals("IGAMXXXXXXXLETRTIVEEVENWASACAYFSIONESSEDNAMNW", testCipher.cipherText);
+    }
+
+    @Test
+    public void TestEncryptString6(){
+        Cipher testCipher = new Cipher("\"Why does it say paper jam when there is no paper jam?\" (3, 14) counter-clockwise");
+        testCipher.init();
+        testCipher.encryptString();
+
+        assertEquals("YHWDSSPEAHTRSPEAMXJPOIENWJPYTEOIAARMEHENAR", testCipher.cipherText);
     }
 
 
